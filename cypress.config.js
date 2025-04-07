70ff70a525a042235278468532a4bd7cb61d5265
Hamid Vahed <khaneghah@yahoo.com>
2025-04-07 23:04:52 +0200

    Trigger GitHub Actions

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.demoblaze.com',
+    exit: true, // Stellt sicher, dass der Cypress-Runner nach dem Test beendet wird
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

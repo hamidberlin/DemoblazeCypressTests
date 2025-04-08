551827d87cfab64262515d648f1400fa35d20ff0
Hamid Vahed <khaneghah@yahoo.com>
2025-04-08 15:40:57 +0200

    Updated cypress.config.js for screenshots

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -5,5 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+     // Screenshots bei Fehlern aufnehmen
+    screenshotOnRunFailure: true,
+     // Ordner für Screenshots
+    screenshotsFolder: "cypress/screenshots",
+    // Optional: Videos von Tests aufnehmen
+    video: true, 
+    // Optional: Video-Kompression einstellen
+    videoCompression: 32, 
+    // Optional: Breite der Screenshots einstellen
+    screenshotWidth: 1280,  
+     // Optional: Höhe der Screenshots einstellen
+    screenshotHeight: 720, 
   },
 });
